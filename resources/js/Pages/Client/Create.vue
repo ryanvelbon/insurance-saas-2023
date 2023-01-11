012df3371452f8a3e570ff89883561773e36c6d7
Ryan <vellabonelloryan@gmail.com>
2023-01-11 10:34:53 +0800

    adds form to Client/Create.vue

--- resources/js/Pages/Client/Create.vue
+++ resources/js/Pages/Client/Create.vue
@@ -12,6 +12,8 @@ const form = useForm({
   email: '',
   phone: '',
   type: '',
+  first_name: '',
+  last_name: '',
 })
 
 const submit = () => {
@@ -27,8 +29,65 @@ const submit = () => {
   <AppLayout>
     <Head title="Add a client" />
 
-    <form @submit.prevent="submit">
-      
-    </form>
+    <div class="overflow-hidden bg-white py-16 px-6 lg:px-8 lg:py-24">
+      <div class="relative mx-auto max-w-xl">
+        <div class="text-center">
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+          <p class="mt-4 text-lg leading-6 text-gray-500">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+        </div>
+        <div class="mt-12">
+          <form @submit.prevent="submit" class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+            <div>
+              <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
+              <div class="mt-1">
+                <input
+                  id="first_name"
+                  name="first_name"
+                  type="text"
+                  class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  v-model="form.first_name"
+                  autocomplete="given-name"
+                />
+              </div>
+            </div>
+            <div>
+              <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
+              <div class="mt-1">
+                <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="company" class="block text-sm font-medium text-gray-700">Company</label>
+              <div class="mt-1">
+                <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+              <div class="mt-1">
+                <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="phone-number" class="block text-sm font-medium text-gray-700">Phone Number</label>
+              <div class="relative mt-1 rounded-md shadow-sm">
+                <div class="absolute inset-y-0 left-0 flex items-center">
+                  <label for="country" class="sr-only">Country</label>
+                  <select id="country" name="country" class="h-full rounded-md border-transparent bg-transparent py-0 pl-4 pr-8 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500">
+                    <option>US</option>
+                    <option>CA</option>
+                    <option>EU</option>
+                  </select>
+                </div>
+                <input type="text" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-gray-300 py-3 px-4 pl-20 focus:border-indigo-500 focus:ring-indigo-500" placeholder="+1 (555) 987-6543" />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <button type="submit" class="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </AppLayout>
 </template>
