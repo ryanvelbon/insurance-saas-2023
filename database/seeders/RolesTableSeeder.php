d34498fe7ad83c5a2826ce56d36a6b3773c4b9e3
Ryan <vellabonelloryan@gmail.com>
2023-10-10 12:20:06 +0800

    refactor: removes unnecessary variable assignment

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -10,30 +10,29 @@ class RolesTableSeeder extends Seeder
 {
     public function run()
     {
-        $role = Role::create([
+        Role::create([
             'id' => 1,
             'title' => 'Admin',
             'description' => 'Oversees all platform functionalities, configurations, and user management to ensure smooth operations of the SaaS.',
-        ]);
-        $permissions = Permission::all();
-        $role->permissions()->sync($permissions->pluck('id'));
+        ])->permissions()->sync(
+            Permission::all()
+        );
 
 
-        $role = Role::create([
+        Role::create([
             'id' => 2,
             'title' => 'User',
             'description' => 'A general user with access to basic features of this software.',
-        ]);
-        $permissions = Permission::all()->reject(function ($permission) {return preg_match('/^(user|role|permission|team)_/', $permission->title); });
-        $role->permissions()->sync($permissions);
+        ])->permissions()->sync(
+            Permission::all()->reject(function ($permission) {return preg_match('/^(user|role|permission|team)_/', $permission->title); })
+        );
 
 
-        $role = Role::create([
+        Role::create([
             'id' => 3,
             'title' => 'Team Leader',
             'description' => 'Supervises and coordinates the activities of a specific team, ensuring efficient workflow and goal attainment.',
-        ]);
-        $role->attachPermissions([
+        ])->attachPermissions([
             'profile_password_edit',
             'person_create',
             'person_edit',
@@ -60,25 +59,29 @@ class RolesTableSeeder extends Seeder
             'team_show'
         ]);
 
-        $role = Role::create([
+
+        Role::create([
             'id' => 5,
             'title' => 'Account Manager',
             'description' => 'Manages client portfolios, ensuring their insurance needs are met and addressing any concerns or changes required in their policies.',
         ]);
 
-        $role = Role::create([
+
+        Role::create([
             'id' => 6,
             'title' => 'Underwriter',
             'description' => 'Assesses the risks associated with potential clients or claims and decides on the terms and conditions of insurance policies.',
         ]);
 
-        $role = Role::create([
+
+        Role::create([
             'id' => 7,
             'title' => 'Claims Adjuster',
             'description' => 'Evaluates and processes insurance claims, determining the payout amounts and ensuring claims are settled fairly and efficiently.',
         ]);
 
-        $role = Role::create([
+
+        Role::create([
             'id' => 8,
             'title' => 'Risk Manager',
             'description' => 'Analyzes potential risks and advises on best insurance policies or preventive measures to mitigate potential threats.',
