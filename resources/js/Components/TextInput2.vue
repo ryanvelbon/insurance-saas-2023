7761a66e33f26cf98005dbd1a736533eccf7f665
Ryan <vellabonelloryan@gmail.com>
2023-10-19 00:45:47 +0800

    feat: alternate TextInput component (with label & error message)

---
+++ resources/js/Components/TextInput2.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { v4 as uuid } from 'uuid'
+
+defineProps(['modelValue', 'label', 'error'])
+
+defineEmits(['update:modelValue'])
+
+const input = ref(null)
+const id = `text-input-${uuid()}`
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus()
+    }
+})
+
+defineExpose({ focus: () => input.value.focus() })
+</script>
+
+<template>
+    <div class="hidden"><!-- Keep this to avoid Vue's attribute inheritance --></div>
+    <div class="flex flex-col">
+        <label v-if="label" :for="id" class="block font-medium text-sm text-gray-700">{{ label }}</label>
+        <input
+            :id="id"
+            v-bind="$attrs"
+            class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            ref="input"
+        />
+        <p v-if="error" class="mt-2 text-sm text-red-600">{{ error }}</p>
+    </div>
+</template>
--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -1,10 +1,7 @@
 <script setup>
 import { Head, useForm } from '@inertiajs/inertia-vue3'
 import AppLayout from '@/Layouts/AppLayout.vue'
-import DropdownA from '@/Components/DropdownA.vue'
-import InputLabel from '@/Components/InputLabel.vue'
-import InputError from '@/Components/InputError.vue'
-import TextInput from '@/Components/TextInput.vue'
+import TextInput from '@/Components/TextInput2.vue'
 import { useStaticDataStore } from '@/stores/staticData'
 
 const props = defineProps({
@@ -33,17 +30,11 @@ const store = () => {
     <Head title="Create a New User" />
     <AppLayout>
         <form @submit.prevent="store" class="bg-gray-200">
-            <div class="bg-red-100">
-                <InputError :message="form.errors.name" />
-                <InputError :message="form.errors.roles" />
-                <InputError :message="form.errors.email" />
-                <InputError :message="form.errors.password" />
-            </div>
             <div class="grid grid-cols-2 gap-8 p-4">
 
-                <TextInput v-model="form.name" required />
-                <TextInput v-model="form.email" required />
-                <TextInput v-model="form.password" type="password" autocomplete="new-password" required />
+                <TextInput v-model="form.name" :error="form.errors.name" label="Name"  type="text" required />
+                <TextInput v-model="form.email" :error="form.errors.email" label="Email" type="email" required />
+                <TextInput v-model="form.password" :error="form.errors.password" label="Password" type="password" autocomplete="new-password" required />
 
                 <div class="col-span-2">
                     <div v-for="role in staticData.dropdowns.roles" :key="role.value">
