a085abe06d91453f927777f19fab2386a5181312
Ryan <vellabonelloryan@gmail.com>
2023-10-20 00:30:56 +0800

    feat: adds some helper methods to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Enums\RoleTitle;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -32,18 +34,66 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function getIsAdminAttribute()
-    {
-        return $this->roles()->where('id', 1)->exists();
-    }
-
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
     }
 
-    public function team()
+    public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class, 'team_id');
+    }
+
+    public function hasRole(RoleTitle $role): bool
+    {
+        return $this->roles()->where('title', $role->value)->exists();
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->hasRole(RoleTitle::ADMIN);
+    }
+
+    public function isTeamLeader(): bool
+    {
+        return $this->hasRole(RoleTitle::TEAM_LEADER);
+    }
+
+    public function isSalesAgent(): bool
+    {
+        return $this->hasRole(RoleTitle::SALES_AGENT);
+    }
+
+    public function isAccountManager(): bool
+    {
+        return $this->hasRole(RoleTitle::ACCOUNT_MANAGER);
+    }
+
+    public function isUnderwriter(): bool
+    {
+        return $this->hasRole(RoleTitle::UNDERWRITER);
+    }
+
+    public function isClaimsAdjuster(): bool
+    {
+        return $this->hasRole(RoleTitle::CLAIMS_ADJUSTER);
+    }
+
+    public function isRiskManager(): bool
+    {
+        return $this->hasRole(RoleTitle::RISK_MANAGER);
+    }
+
+    public function permissions(): array
+    {
+        return $this->roles()->with('permissions')->get()
+            ->map(function ($role) {
+                return $role->permissions->pluck('title');
+            })->flatten()->values()->unique()->toArray();
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return in_array($permission, $this->permissions(), true);
     }
 }
