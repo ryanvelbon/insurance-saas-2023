7bc1950f4e6df56dab2a7e7e8fa685b3cd355d18
Ryan <vellabonelloryan@gmail.com>
2023-10-20 10:02:28 +0800

    refactor: modularizes user creation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,10 +10,14 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
-        // +++ create admin user +++
+        $this->createAdmin();
+        $this->createEndUsers();
+    }
 
+    public function createAdmin()
+    {
         $admin = User::create([
             'id'             => 1,
             'name'           => 'Admin',
@@ -23,18 +27,18 @@ class UserSeeder extends Seeder
         ]);
 
         $admin->roles()->sync(Role::where('title', RoleTitle::ADMIN)->first()->id);
+    }
 
-        // +++ create end-users +++
-
-        foreach (['Team A', 'Team B'] as $teamName) {
+    public function createEndUsers()
+    {
+        foreach (['Team A', 'Team B', 'Team C', 'Team D'] as $teamName) {
 
             $team = Team::create(['name' => $teamName]);
 
             $users = User::factory()
-                        ->count(3)
+                        ->count(5)
                         ->create(['team_id' => $team->id]);
 
-            // assign Sales Agent role
             foreach ($users as $user) {
                 $user->roles()->sync(Role::where('title', RoleTitle::SALES_AGENT)->first()->id);
             }
@@ -42,9 +46,5 @@ class UserSeeder extends Seeder
             $team->owner_id = $users[0]->id;
             $team->save();
         }
-
-        // *TEMP* adds admin to Team B
-        $admin->team_id = 2;
-        $admin->save();
     }
 }
